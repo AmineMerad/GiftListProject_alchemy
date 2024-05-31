027950eda5fd6e3fa5c542c8b26c8bfb859e5f83
AmineMerad <130429769+AmineMerad@users.noreply.github.com>
2024-05-31 13:50:46 +0100

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const verifyProof = require('../utils/verifyProof');
+const MerkleTree = require('../utils/MerkleTree');
+
+const port = 1225;
+
+const app = express();
+app.use(express.json());
+
+// TODO: hardcode a merkle root here representing the whole nice list
+const merkleTree = new MerkleTree(niceList)
+// paste the hex string in here, without the 0x prefix
+const MERKLE_ROOT = merkleTree.getRoot();
+
+app.post('/gift', (req, res) => {
+  // grab the parameters from the front-end here
+  const { name, proof } = req.body
+  const body = req.body;
+
+  // TODO: prove that a name is in the list 
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if (isInTheList) {
+    res.send("You got a toy robot!");
+  }
+  else {
+    res.send("You are not on the list :(");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
